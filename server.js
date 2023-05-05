e1303f4f8d8dff02596b3fa4e54ddf93330ccc31
itarek99 <tarekulislam7060@gmail.com>
2023-05-05 15:17:50 +0600

    server connected

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
-const DBConnect = require("./utils/dbConnect");
+// const DBConnect = require("./utils/dbConnect");
 
 const app = require("./app");
 
 // database connection
-DBConnect();
+
+mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
+  console.log("db connection successful");
+});
+
+// DBConnect();
 
 // server
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-  console.log(`App is running on port ${port}`.yellow.bold);
+  console.log(`app is running on port ${port}`.yellow.bold);
 });
-
